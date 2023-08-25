47c650816070635c0a7bb11c7f5647cb1198889c
Flopiras <Flopiras@yahoo.it>
2023-08-25 17:40:23 +0200

    edit cards

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,18 +4,23 @@
 
 @section('main')
 <section id="current-series">
-    <div class="container cards-container">
-        @foreach($comics as $comic)
+    <div class="container mt-4">
+        <div class="row row-cols-4">
+            @foreach($comics as $comic)
+            <div class="col">
         {{-- cards --}}
-        <div class="card">
-            <a href="{{route('comics.detail', $loop->index)}}">
-                <figure>
-                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                    <figcaption>{{ $comic['title'] }}</figcaption>
-                </figure>
-            </a>
+        <div class="card mb-4" style="width: 18rem;">
+            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="{{ $comic['title'] }}">
+            <div class="card-body">
+              <h5 class="card-title">{{ $comic['title'] }}</h5>
+              <p class="card-text">{{ $comic['description'] }}</p>
+              <a href="{{url("/comics.{comic}")}}" class="btn btn-primary">Read more</a>
+            </div>
+          </div>
         </div>
         @endforeach
+        </div>
+        
         <div class="button">LOAD MORE</div>
     </div>
 </section>
