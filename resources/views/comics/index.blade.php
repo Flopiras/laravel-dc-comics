f6955673afa26e97d4fd0c91acc96cc7fe244abe
Flopiras <Flopiras@yahoo.it>
2023-08-25 16:43:44 +0200

    add main

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,5 +3,21 @@
 @section('title', 'Comics')
 
 @section('main')
-    
+<section id="current-series">
+    <div class="container cards-container">
+        @foreach($comics as $comic)
+        {{-- cards --}}
+        <div class="card">
+            <a href="{{route('comics.detail', $loop->index)}}">
+                <figure>
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    <figcaption>{{ $comic['title'] }}</figcaption>
+                </figure>
+            </a>
+        </div>
+        @endforeach
+        <div class="button">LOAD MORE</div>
+    </div>
+</section>
+
 @endsection
