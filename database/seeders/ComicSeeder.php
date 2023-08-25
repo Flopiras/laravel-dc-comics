3be9d5430428e34e2aac67ccc74e6c36cca066a3
Flopiras <Flopiras@yahoo.it>
2023-08-25 18:08:16 +0200

    add config.comics in db

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $new_comic = new Comic();
+
+            $new_comic->fill($comic);
+
+            $new_comic->artists = implode($new_comic->artists);
+            $new_comic->writers = implode($new_comic->writers);
+
+            $new_comic->save();
+        }
     }
 }
