f710c573b262ec35a0fa48dfc9048c103ba8c8d1
Flopiras <Flopiras@yahoo.it>
2023-08-25 18:38:34 +0200

    add comic.show page

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('title', $comic->title)
+
+@section('main')
+<section>
+    <div class="container ">
+        <div class="card mt-4">
+
+            <div class="card-header text-center mb-3">
+                <h1>{{ $comic->title }}</h1>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-3">
+                    <img class="img-fluid" src="{{ $comic['thumb'] }}"  alt="{{ $comic['title'] }}">
+                    </div>
+                    <div class="col-9">
+                    <p class="card-text">{{ $comic['description'] }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <a href="{{route('comics.index')}}" class="btn btn-primary mt-4">Back</a>
+    </div>
+</section>
+
+@endsection
