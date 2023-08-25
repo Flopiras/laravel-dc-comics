a5eca62cea630974c71603bc711e0fe99b6528df
Flopiras <Flopiras@yahoo.it>
2023-08-25 18:07:34 +0200

    add seeder columns

--- database/migrations/2023_08_25_140448_create_comics_table.php
+++ database/migrations/2023_08_25_140448_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150)->unique();
+            $table->text('description')->nullable();
+            $table->text('thumb', 300)->nullable();
+            $table->string('price', 10)->nullable();
+            $table->string('series', 50)->nullable();
+            $table->string('sale_date', 50)->nullable();
+            $table->string('type', 40)->nullable();
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
             $table->timestamps();
         });
     }
